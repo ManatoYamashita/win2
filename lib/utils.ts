9552ba496b26eaed4fe9bdc0a6c61f740f31f369
ManatoYamashita <info@manapuraza.com>
2025-10-28 12:33:33 +0900

    FEATURE: BlogのCTAボタンの実装

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+/**
+ * 日付を YYYY年MM月DD日 形式にフォーマット
+ */
+export function formatDate(dateString: string): string {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  return `${year}年${month}月${day}日`;
+}
